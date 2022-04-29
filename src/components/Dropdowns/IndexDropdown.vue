32e918d61675328752761680ddb8ea9bffedd0ee
David Arboleda <dasvv7@gmail.com>
2022-04-28 21:41:02 -0500

    Update IndexDropdown.vue
    ASDASD

--- src/components/Dropdowns/IndexDropdown.vue
+++ src/components/Dropdowns/IndexDropdown.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a
-      class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+      class="bg-blue-500  active:bg-blue-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
       href="#pablo"
       ref="btnDropdownRef"
       @click="toggleDropdown($event)"
